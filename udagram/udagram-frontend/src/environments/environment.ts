557433c97e2fa4a2d2ff1e5f4818ee0acc76817a
Mohamed Saeed <mohamed1707171@miuegypt.edu.eg>
2023-02-28 01:10:01 +0200

    updated frontend env

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -4,8 +4,10 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  // apiHost: 'http://localhost:8080/api/v0'
+
+  apiHost: "Udagramapi-env.eba-ya6eyruu.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
